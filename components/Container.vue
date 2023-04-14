bd4e47969867c4a70bc309ca370f0205fffe9517
Victorakinwunmi <adexvictor94@gmail.com>
2023-04-14 08:25:30 -0700

    i added padding to the header

--- components/Container.vue
+++ components/Container.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed z-10">
     <div class="lg:block w-full bg-white shadow-xl lg:shadow-sm rounded-b-md py-5 z-10 fixed">
-      <div class="flex justify-between items-center max-w-8xl mx-auto px-4 md:px-6 lg:py-2 lg:px-2 xl:px-0">
+      <div class="flex justify-between items-center max-w-8xl mx-auto px-4 md:px-6 py-2 lg:px-2 xl:px-0">
         <ul class="items-center gap-10 hidden lg:flex">
           <li class="uppercase text-sm">how it works</li>
           <li class="uppercase text-sm">Faqs</li>
